6677d5549007c1d67b83f9cad65c77c48101aa6b
jasonchanwq <jasonchanwq@gmail.com>
2024-04-23 17:30:42 +0800

    Edit Background

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,11 +1,26 @@
-import React from "react";
-import logo from "../assets/logo.png";
+import React, { useState, useEffect, Children } from "react";
+import image1 from "../assets/image1.jpg";
+import image2 from "../assets/image2.jpg";
+import image3 from "../assets/image3.jpg";
+import image4 from "../assets/image4.jpg";
 
-const Background = () => {
+const Background = ({}) => {
+  const images = [image1, image2, image3, image4]; // Array of imported images
+  const [backgroundImage, setBackgroundImage] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setBackgroundImage((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000); // Change image every 5 seconds
+
+    return () => clearInterval(interval);
+  }, []); // Run effect only once when component mounts
+
   return (
     <div
+      className="background-image"
       style={{
-        backgroundImage: `url(${logo})`,
+        backgroundImage: `url(${images[backgroundImage]})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
         /* Add other background properties as needed */
