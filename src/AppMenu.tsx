7dcac170c30cf33e6ce6dc676e8ff18b4ac7e6a2
jr2tan5 <jr2tan_5@hotmail.com>
2024-04-24 03:19:24 +0800

    optimised AppMenu.tsx

--- src/AppMenu.tsx
+++ src/AppMenu.tsx
@@ -1,11 +1,11 @@
 import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
-import NavigationBar from "./components/NavigationBar";
+import Background from "./components/Background";
 
 function AppMenu() {
   //return <Button colorScheme="blue">Button</Button>;
   return (
     <>
-      <Background></Background>
+      <Background/>
     </>
   );
 }
