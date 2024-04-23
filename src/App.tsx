77d405eb4ef8c4613d9836894dc9bd843b0cf186
jasonchanwq <jasonchanwq@gmail.com>
2024-04-23 20:08:49 +0800

    Background adjust zoom

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,7 @@ function App() {
   //return <Button colorScheme="blue">Button</Button>;
   return (
     <>
-      <Background>
-        <Grid
-          templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
-        >
-          <GridItem area="nav"> </GridItem>
-          <Show above="lg">
-            <GridItem area="aside" bg="gold">
-              {" "}
-              ignore
-            </GridItem>
-          </Show>
-          <GridItem area="main" bg="coral">
-            {" "}
-            ignore2
-          </GridItem>
-        </Grid>
-      </Background>
+      <Background></Background>
     </>
   );
 }
