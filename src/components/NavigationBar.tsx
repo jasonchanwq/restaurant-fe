24c1f4e49b8a901a6c4bd79c8903e9cb65ed96f7
jr2tan5 <jr2tan_5@hotmail.com>
2024-04-24 16:14:18 +0800

    Test of Navbar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,18 +1,21 @@
 import { HStack, Text } from "@chakra-ui/react";
+import { Link as ReactRouterLink } from 'react-router-dom'
+import { Link as ChakraLink, LinkProps } from '@chakra-ui/react'
 import React from "react";
 import logo from "../assets/logo.png";
+
 
 const NavigationBar = () => {
   return (
     <HStack justifyContent="space-between" padding="50px">
       <img src={logo} style={{ width: "100px", height: "100px" }} />
       <Text>Navigation Bar Component</Text>
-      <Text>Button</Text>
-      <Text>Button</Text>
-      <Text>Button</Text>
-      <Text>Button</Text>
-      <Text>Button</Text>
-      <Text>Button</Text>
+      <Text>Home</Text>
+      <ChakraLink as={ReactRouterLink} to='/home'>
+        Home
+      </ChakraLink>
+      <Text>Gallery</Text>
+      <Text>Contacts</Text>
     </HStack>
   );
 };
